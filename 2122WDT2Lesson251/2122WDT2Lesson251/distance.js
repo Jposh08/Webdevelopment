13d87eaa1f07efa9ba80e7ef2078532324e0f78a
Jposh08 <150818210+Jposh08@users.noreply.github.com>
2025-02-05 13:47:08 -0500

    Update distance.js

--- 2122WDT2Lesson251/2122WDT2Lesson251/distance.js
+++ 2122WDT2Lesson251/2122WDT2Lesson251/distance.js
@@ -1,5 +1,6 @@
 /* Challenge 2: Create a function to serve as the event handler for the distance UI. Guidelines,
       1) Create variables and retrieve the information from the text inputs you created in Challenge 1.
+      let 
       2) Perform the necessary calculations
       3) Display the results in the appropriate element
 */
